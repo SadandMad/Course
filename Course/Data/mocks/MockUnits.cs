361fac19fb5869461169b2b37ba7277b0782dc60
Vendi <chemodanik807@gmail.com>
2021-05-15 05:44:26 +0300

    Razor, CSS...

--- Course/Data/mocks/MockUnits.cs
+++ Course/Data/mocks/MockUnits.cs
@@ -15,16 +15,16 @@ namespace Course.Data.mocks
                 return new List<Unit> {
                     new Unit { Name = "Primaris Captain", shortDesk = "Первый парень на селе",
                         longDesk = "Капитаны Космодесанта – одновременно превосходные воители и одарённые полководцы, командующие в бою ротами Адептус Астартес.",
-                        img = "https://www.games-workshop.com/resources/catalog/product/920x950/99120101179_PrimarisCaptain01.jpg", price = 5, isFavourite = true, available = 1, Category = _CategoryUnits.getAllCategories.First() },
+                        img = "/img/99120101179_PrimarisCaptain01.webp", price = 5, isFavourite = true, available = 1, Category = _CategoryUnits.getAllCategories.First() },
                     new Unit { Name = "Intercessor Squad", shortDesk = "Стоим-стреляем",
                         longDesk = "Заступники — это ядро любой ударной группировки космодесантников-примарис.",
-                        img = "https://www.games-workshop.com/resources/catalog/product/920x950/99120101190_PrimarisIntercessors01.jpg", price = 5, isFavourite = true, available = 3, Category = _CategoryUnits.getAllCategories.ElementAt(2) },
+                        img = "/img/99120101190_PrimarisIntercessors01.webp", price = 5, isFavourite = true, available = 3, Category = _CategoryUnits.getAllCategories.ElementAt(2) },
                     new Unit { Name = "Hellblaster Squad", shortDesk = "Главное - не кинуть кол",
                         longDesk = "Изничтожители — отряды, составленные из превосходных стрелков, обеспечивающие группировкам космодесантников-примарис опустошительную огневую мощь на средней дистанции.",
-                        img = "https://www.games-workshop.com/resources/catalog/product/920x950/99120101189_PrimarisHellblasters01.jpg", price = 8, isFavourite = true, available = 3, Category = _CategoryUnits.getAllCategories.Last() },
+                        img = "/img/99120101189_PrimarisHellblasters01.webp", price = 8, isFavourite = true, available = 3, Category = _CategoryUnits.getAllCategories.Last() },
                     new Unit { Name = "Deathwing Terminators", shortDesk = "А ты часом не Падший?",
                         longDesk = "Отделения терминаторов – неукротимые воины, разносящие врага на части шквальным огнём штормболтеров.",
-                        img = "https://www.games-workshop.com/resources/catalog/product/920x950/99120101096_DeathwingTerminators01.jpg", price = 9, isFavourite = false, available = 0, Category = _CategoryUnits.getAllCategories.ElementAt(3) }
+                        img = "/img/99120101096_DeathwingTerminators01.jpg", price = 9, isFavourite = false, available = 0, Category = _CategoryUnits.getAllCategories.ElementAt(3) }
                 };
             }
         }
