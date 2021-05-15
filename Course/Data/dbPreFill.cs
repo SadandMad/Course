7db4a3db92e9ebdd552e88bc2516639c9d3b3362
Vendi <chemodanik807@gmail.com>
2021-05-15 07:20:35 +0300

    DB works)

--- Course/Data/dbPreFill.cs
+++ Course/Data/dbPreFill.cs
@@ -17,7 +17,52 @@ namespace Course.Data
 
             if (!content.Unit.Any())
             {
-                content.AddRange(Units.Select(c => c.Value));
+                content.AddRange(
+                    new Unit
+                    {
+                        Name = "Primaris Captain",
+                        shortDesk = "Первый парень на селе",
+                        longDesk = "Капитаны Космодесанта – одновременно превосходные воители и одарённые полководцы, командующие в бою ротами Адептус Астартес.",
+                        img = "/img/99120101179_PrimarisCaptain01.webp",
+                        price = 5,
+                        isFavourite = true,
+                        available = 1,
+                        Category = Categories["HQ"]
+                    },
+                    new Unit
+                    {
+                        Name = "Intercessor Squad",
+                        shortDesk = "Стоим-стреляем",
+                        longDesk = "Заступники — это ядро любой ударной группировки космодесантников-примарис.",
+                        img = "/img/99120101190_PrimarisIntercessors01.webp",
+                        price = 5,
+                        isFavourite = true,
+                        available = 3,
+                        Category = Categories["Troops"]
+                    },
+                    new Unit
+                    {
+                        Name = "Hellblaster Squad",
+                        shortDesk = "Главное - не кинуть кол",
+                        longDesk = "Изничтожители — отряды, составленные из превосходных стрелков, обеспечивающие группировкам космодесантников-примарис опустошительную огневую мощь на средней дистанции.",
+                        img = "/img/99120101189_PrimarisHellblasters01.webp",
+                        price = 8,
+                        isFavourite = true,
+                        available = 3,
+                        Category = Categories["Heavy Support"]
+                    },
+                    new Unit
+                    {
+                        Name = "Deathwing Terminators",
+                        shortDesk = "А ты часом не Падший?",
+                        longDesk = "Отделения терминаторов – неукротимые воины, разносящие врага на части шквальным огнём штормболтеров.",
+                        img = "/img/99120101096_DeathwingTerminators01.jpg",
+                        price = 9,
+                        isFavourite = false,
+                        available = 0,
+                        Category = Categories["Elites"]
+                    }
+                );
             }
 
             content.SaveChanges();
@@ -26,7 +71,7 @@ namespace Course.Data
         private static Dictionary<string, Category> category;
         public static Dictionary<string, Category> Categories
         {
-            /*get {
+            get {
                 if(category == null) {
                     var list = new Category[] {
                         new Category { Name = "HQ", Desc = "Командующие персонажи вашей армии" },
@@ -41,35 +86,6 @@ namespace Course.Data
                         category.Add(el.Name, el);
                 }
                 return category;
-            }*/
-            get
-            {
-                if (category == null)
-                {
-                    MockCategory temp = null;
-
-                    category = new Dictionary<string, Category>();
-                    foreach (Category el in temp.getAllCategories)
-                        category.Add(el.Name, el);
-                }
-                return category;
-            }
-        }
-        private static Dictionary<string, Unit> unit;
-
-        public static Dictionary<string, Unit> Units
-        {
-            get
-            {
-                if (unit == null)
-                {
-                    MockUnits temp = null;
-
-                    unit = new Dictionary<string, Unit>();
-                    foreach (Unit el in temp.getUnits)
-                        unit.Add(el.Name, el);
-                }
-                return unit;
             }
         }
     }
